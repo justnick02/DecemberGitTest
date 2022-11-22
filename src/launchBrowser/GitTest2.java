81bb8ce2029292e052f4908218b6cf25e1394e9d
justnick02 <justnick02@gmail.com>
2022-11-22 18:41:13 -0500

    Forth Commit

--- src/launchBrowser/GitTest2.java
+++ src/launchBrowser/GitTest2.java
@@ -2,4 +2,15 @@ package launchBrowser;
 
 public class GitTest2 {
 
+	public static void main(String[] args) {
+		
+	}
+	
+	public void gitTest() {
+		
+		System.out.println("Git Test1");
+		System.out.println("Git Test2");
+		System.out.println("Git Test3");
+		
+	}
 }
